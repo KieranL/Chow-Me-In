692887836971beb345c34455ff031e91fdd32449
Eric Chubaty <echubaty@gmail.com>
2018-01-23 21:45:28 -0600

    trying to get things working and stuff

--- runserver.py
+++ runserver.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
+from flask import Flask
 import subprocess
+
+application = Flask(__name__)
 
 def main():
 	# Make sure requirements are up to date prior to launching server
-	subprocess.run(['sudo', '-H', 'pip3', 'install', '-r', 'server/requirements.txt'])
+	subprocess.run(['sudo', '-H', 'pip-3.6', 'install', '-r', 'server/requirements.txt'])
 
 	# Run Flask server
 	subprocess.run(['python3', 'server/server.py'])
