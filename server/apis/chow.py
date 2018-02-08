37e73238f32150d84fdd3182478b0194127e4326
Kieran Loewen <kieranloewen@live.com>
2018-02-08 09:38:49 -0600

    add get chow endpoint

--- server/apis/chow.py
+++ server/apis/chow.py
@@ -6,3 +6,7 @@ chow_api_bp = Blueprint('chow', __name__)
 @chow_api_bp.route('/chows', methods=['GET'])
 def get_chows() :
     return ChowController.get_chows()
+
+@chow_api_bp.route('/chow/<int:chow_id>', methods=['GET'])
+def get_chow(chow_id) :
+    return ChowController.get_chow(chow_id)
--- server/controllers/chow.py
+++ server/controllers/chow.py
@@ -3,4 +3,10 @@ from flask import jsonify
 class ChowController():
     @classmethod
     def get_chows(cls):
-        return jsonify({"success":{"chows": []}})
+        chows = [{"id": 1}, {"id": 2}]
+        return jsonify({"success":{"chows": chows}}) #TODO use real data
+    
+    @classmethod
+    def get_chow(cls, chow_id):
+        chow = {"id": chow_id}
+        return jsonify({"success":{"chow": chow}}) #TODO use real data
