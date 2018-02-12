22d2b10fd1e59564dd5233627f515523228ba6c3
tiffcj <tiffanyjiang598@gmail.com>
2018-02-11 18:28:59 -0600

    Added routing to create chow page

---
+++ web/src/app/chow-form/chow-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chow-form',
+  templateUrl: './chow-form.component.html',
+  styleUrls: ['./chow-form.component.css']
+})
+export class ChowFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
