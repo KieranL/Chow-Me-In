2ed3cdd3b73c346c4214842728275925756d027c
Eric Chubaty <echubaty@gmail.com>
2018-02-25 01:37:20 -0600

    Login now updates page components async, woo

--- server/chowmein/apis/user.py
+++ server/chowmein/apis/user.py
@@ -5,17 +5,13 @@ import json
 user_api_bp = Blueprint('user', __name__)
 
 @user_api_bp.route('/user/token/<string:userToken>', methods=['GET'])
-def get_username(userToken):
-    return UserController.get_username(userToken)
+def get_user(userToken):
+    return UserController.get_user(userToken)
 
 @user_api_bp.route('/user', methods=['POST'])
 def create_user() :
     data = json.loads(request.form['user'])
     return UserController.create_user(data)
-
-@user_api_bp.route('/user/<int:user_id>', methods=['GET'])
-def get_user(user_id) :
-    return UserController.get_user(user_id)
 
 @user_api_bp.route('/user/<int:user_id>', methods=['POST'])
 def update_user(user_id) :
