ae15f8e4529db864763d53acd0434c15ab6e4f7b
Eric Chubaty <echubaty@gmail.com>
2018-02-25 20:10:26 -0600

    Updated clientid for login

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent {
   login(event) {
   	// Change location to cognito signin url
   	// Callback URL must be fixed - whitelisted in AWS cognito console
-  	window.location.assign("https://chowmein.auth.us-east-2.amazoncognito.com/login?response_type=token&client_id=7pfks1vh7tn778mhqo107ha9g7&redirect_uri=https://chowme-in.com/login");
+  	window.location.assign("https://chowmein.auth.us-east-2.amazoncognito.com/login?response_type=token&client_id=6s28e9fcoakqhdm5nrud946d5p&redirect_uri=https://chowme-in.com/login");
 	}
 
   logout(event) {
