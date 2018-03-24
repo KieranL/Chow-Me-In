be851b09ba463ca59c738993db5000b8a49eb193
tiffcj <tiffanyjiang598@gmail.com>
2018-03-24 01:17:35 -0500

    Add search bar

--- web/src/app/material/material.module.ts
+++ web/src/app/material/material.module.ts
@@ -5,6 +5,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
 
 // Import and export all required Angular Material components above and below
 
@@ -15,14 +16,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   exports: [
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule
   ]
 })
 export class MaterialModule { }
