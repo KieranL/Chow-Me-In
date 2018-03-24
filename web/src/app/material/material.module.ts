483361898bd08aff4afff918f6850a76b0fcb3d4
tiffcj <tiffanyjiang598@gmail.com>
2018-03-23 20:15:22 -0500

    Chow form styling + material buttons

--- web/src/app/material/material.module.ts
+++ web/src/app/material/material.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 // Import and export all required Angular Material components above and below
 
@@ -9,11 +11,15 @@ import { MatCardModule } from '@angular/material/card';
   imports: [
     CommonModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   exports: [
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class MaterialModule { }
